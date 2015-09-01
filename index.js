615f8846cd4c40c6b7ab71f571181bf4683c1ea3
Kyle Mitchell <kyle@kemitchell.com>
2015-08-31 18:33:47 -0700

    Condense code style

--- index.js
+++ index.js
@@ -1,60 +1,50 @@
-var groupSeries = require('commonform-group-series');
+var groupSeries = require('commonform-group-series')
 
 var number = function(form, numberings, headings, parentNumbering) {
-  var seriesNumber = 0;
-  var elementIndex = 0;
+  var seriesNumber = 0
+  var elementIndex = 0
 
-  var groups = groupSeries(form);
+  var groups = groupSeries(form)
 
   // Count series, to provide X of Y numberings later.
   var seriesCount = groups
-    .filter(function(group) { return group.type === 'series'; })
-    .length;
+    .filter(function(group) { return group.type === 'series' })
+    .length
 
   // Compute numberings.
   groups.forEach(function(group) {
     if (group.type !== 'series') {
-      elementIndex += group.content.length;
-    } else {
-      seriesNumber++;
+      elementIndex += group.content.length }
+    else {
+      seriesNumber++
       if (!numberings.hasOwnProperty('content')) {
-        numberings.content = {};
-      }
-      var contentNumberings = numberings.content;
+        numberings.content = {} }
+      var contentNumberings = numberings.content
       group.content.forEach(function(child, childIndex, content) {
-        var index = elementIndex++;
+        var index = elementIndex++
 
         // Numbering
         var childNumbering = parentNumbering.concat([{
-          series: {number: seriesNumber, of: seriesCount},
-          element: {number: childIndex + 1, of: content.length}
-        }]);
-        var childNumberings = contentNumberings[index] = {};
-        childNumberings.numbering = childNumbering;
+          series:  { number: seriesNumber, of: seriesCount },
+          element: { number: childIndex + 1, of: content.length } }])
+        var childNumberings = contentNumberings[index] = { }
+        childNumberings.numbering = childNumbering
 
         // Heading
         if (child.hasOwnProperty('heading')) {
-          var heading = child.heading;
+          var heading = child.heading
           if (!headings.hasOwnProperty(heading)) {
-            headings[heading] = [];
-          }
-          headings[heading].push(childNumbering);
-        }
+            headings[heading] = [ ] }
+          headings[heading].push(childNumbering) }
 
         // Recursion
-        var nextNumberings = {};
-        number(child.form, nextNumberings, headings, childNumbering);
+        var nextNumberings = { }
+        number(child.form, nextNumberings, headings, childNumbering)
         if (nextNumberings.hasOwnProperty('content')) {
-          childNumberings.form = nextNumberings;
-        }
-      });
-    }
-  });
-};
+          childNumberings.form = nextNumberings } }) } }) }
 
 module.exports = function(form) {
-  var numberings = {};
-  var headings = {};
-  number(form, numberings, headings, []);
-  return {form: numberings, headings: headings};
-};
+  var numberings = { }
+  var headings = { }
+  number(form, numberings, headings, [])
+  return { form: numberings, headings: headings } }
